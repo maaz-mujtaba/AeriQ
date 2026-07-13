43af7b0047c5997d484dbb6eaeebdad95a4762c9
Maaz Mujtaba <maaz32418@gmail.com>
2026-07-13 15:03:59 +0530

    Database initialised

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,2 +1,14 @@
-from pydantic import BaseSettings
-from typing import Optional, List
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from typing import Optional
+
+class Settings(BaseSettings):
+    DATABASE_URL : str
+    OPEN_METEO_BASE_URL : str
+    APP_NAME : str
+    DEBUG = Optional[bool] = False
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+settings = Settings()
--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,1 +1,20 @@
-#
+from app.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+engine = create_engine(settings.DATABASE_URL, echo=settings.DEBUG)
+
+SessionLocal = sessionmaker(
+    autocommit = False,
+    autoflus = False,
+    bind = engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
